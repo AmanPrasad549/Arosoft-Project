0f6e1779fb8dacdb5d35c0cb8530f6c4eb5a34a4
AMAN PRASAD <155144416+AmanPrasad549@users.noreply.github.com>
2025-01-02 21:06:24 +0530

    Add files via upload

---
+++ frontend/src/Component/OurStory.js
@@ -0,0 +1,13 @@
+import '../Css/OurStory.css';
+
+const OurStory = () =>{
+    return (
+        <>
+            <div className="sliding-text">
+                <h1>Welcome to My Website!</h1>
+            </div>
+        </>
+    )
+} 
+ 
+export default OurStory ;
