34756020120ca623531d65dbfdd6f77c53a8afcd
AMAN PRASAD <155144416+AmanPrasad549@users.noreply.github.com>
2025-01-02 21:02:16 +0530

    Add files via upload

---
+++ frontend/src/Component/President.js
@@ -0,0 +1,69 @@
+import '../Css/PMassage.css';
+
+const PMassage = () =>{
+   return(
+        <>
+            <div className='bImage'>
+                <div className='container'> 
+                    <h1 className='PMassage'>
+                        PRESIDENT'S MASSAGE
+                    </h1>
+                </div>
+            </div>
+            <div className='container'>
+                <div>
+                    <h1>
+                        Reciving Aword Image
+                    </h1>
+                    <p className='paragraph'>
+                        The award is presented to those who have demonstrated a lifelong commitment to improving
+                         educational opportunities and community engagement.
+                        Dr. Wright's leadership and initiatives at Eureka College have positively
+                         impacted students and the surrounding community, exemplifying the values of dedication and service in education.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <h1 className='Reflection'>
+                    Reflection for the President's Desk
+                </h1>
+            </div>
+            <div className='col-lg-12 vol-md-12 col-sm-12 col-xs-12'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-lg-6 col-md-6 com-sm-12 col-xs-12'>
+                            <p className='paragraph'>
+                                As we navigate the complexities of today’s educational landscape,
+                                I am reminded of the profound responsibility we hold as leaders in academia.
+                                Each day, we are entrusted with the growth and development of not only our students
+                                but also the communities we serve. Our mission extends beyond the classroom; 
+                                it encompasses fostering an environment where curiosity thrives,
+                                diversity is celebrated, and every individual feels valued.
+                            </p>
+                            <p className='paragraph'>
+                                In moments of challenge, I find inspiration in the resilience of our students and faculty.
+                                Their dedication to learning and innovation fuels my commitment to ensuring that our
+                                institution remains a beacon of hope and opportunity. It is our duty to equip our students 
+                                with the skills and knowledge they need to navigate an ever-changing world, 
+                                while also instilling in them a sense of purpose and responsibility toward their communities.
+                            </p>
+                            <p className='paragraph'>
+                                As we look to the future, let us embrace collaboration and inclusivity.
+                                The strength of our college lies in our ability to unite diverse perspectives
+                                and experiences. Together, we can create a vibrant academic community that not 
+                                only prepares our students for successful
+                                careers but also empowers them to become compassionate leaders and engaged citizens.
+                            </p>
+                        </div>
+                        <div className='col-lg-6 col-md-6 com-sm-12 col-xs-12'>
+                            <h1>
+                                College President Image
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+   )
+}
+export default PMassage;
---
+++ frontend/src/Component/SeniorSeconder.js
@@ -0,0 +1,62 @@
+import '../Css/SeniorSeconder.css';
+
+const Ssecondary = () =>{
+    return (
+        <>
+            <div className='bimage'>
+                <div className='container'>
+                    <h1 className='Senior'>
+                        SENIOUR SECONDARY SCHOOL
+                    </h1>
+                </div>
+                
+            </div>
+            <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                            <h1>
+                                Image with teacher
+                            </h1>
+                        </div>
+                        <div className='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                            <h3 className='Seniorsecondaryschool'>
+                                SENIOR SECONDARY SCHOOL
+                            </h3>
+                            <h1 className='Indiatop'>
+                                TOP CBSE <span className='SSchool'>
+                                            SENIOR SECONDARY SCHOOL IN INDIA
+                                        </span>
+                            </h1>
+                            <p className='P_Senior'>
+                               Senior Secondary education represents the pinnacle of foundation learning,
+                               with the two-year period crucial as students transition to professional 
+                               education. The Senior Secondary division of JPS excels in guiding studen
+                               to achieve outstanding results in board examination as well as vasrious Entrance
+                               exams.  
+                            </p>
+                            <p className='P_Senior'>
+                                Jindal boasts some of the best teacher in the state who are dedicated to 
+                                supporting and facilitating students sucess. This achivement is a natural
+                                progression stemming from the strong foundation establised at the Secondary
+                                level. A specially designed teaching model is implemented, with all teacher
+                                adhering to it and emphasizing the importance of a clear understanding off
+                                fundamental concepts. This approach enables students to excel in boath board.
+                            </p>
+                        </div>
+                        {/* 2nd Row */}
+                        <div className='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                            
+                        </div>
+                        <div className='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+        </>
+    )
+} 
+ 
+export default Ssecondary ;
